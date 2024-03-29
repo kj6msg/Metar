a3b16f91a526fcd81a6347c6d66e5299df536d0f
Ryan Clarke <rfclarke@alum.wpi.edu>
2024-03-29 16:31:02 -0400

    Added command-line options.

--- Metar/MetarApp.swift
+++ Metar/MetarApp.swift
@@ -5,22 +5,63 @@
 //  Created by Ryan Clarke on 3/9/24.
 //
 
+import Foundation
+import ArgumentParser
+
 @main
-struct MetarApp
+struct MetarApp: AsyncParsableCommand
 {
-    static func main() async throws
+    static var configuration = CommandConfiguration(
+        abstract: "Fetches a METAR observation.",
+        usage: """
+            Metar [--decoded] <icao>
+            """,
+        discussion: "Prints a METAR observation in either raw or decoded format."
+    )
+    
+    @Flag(help: "Print decoded observation.")
+    var decoded = false
+    
+    @Argument(help: "ICAO ID.")
+    var icao: String
+    
+    mutating func validate() throws
     {
-        var metar = Metar(station: "KPVD")
+        guard icao.containsOnlyLetters && icao.count == 4 else
+        {
+            throw ValidationError("Invalid ICAO ID.")
+        }
+    }
+    
+    mutating func run() async throws
+    {
+        var metar = Metar(station: icao)
         
         do
         {
             try await metar.fetch()
+            
+            if(decoded)
+            {
+                print(metar.decoded)
+            }
+            else
+            {
+                print(metar)
+            }
         }
         catch
         {
-            print("Error: \(error)")
+            print("Unexpected error: \(error).")
         }
-        
-        print(metar.decoded)
+    }
+}
+
+extension String
+{
+    var containsOnlyLetters: Bool
+    {
+        let notLetters = NSCharacterSet.letters.inverted
+        return rangeOfCharacter(from: notLetters, options: String.CompareOptions.literal) == nil
     }
 }
